e6fa2135fdd159f3363297aca7495bb417791410
imSaikirann <neelakantamsaikiran7@gmail.com>
2023-05-15 22:19:03 +0530

    sec c

--- src/components/Dic.jsx
+++ src/components/Dic.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Input, Stack, Button, Heading, Text, Icon , InputGroup,Flex} from '@chakra-ui/react'
+import { Input, Stack, Button, Heading, Text, Icon, InputGroup, Flex } from '@chakra-ui/react'
 import { FaSearch } from "react-icons/fa"
 
 function Dic({ isDarkMode }) {
@@ -35,42 +35,39 @@ function Dic({ isDarkMode }) {
     }
 
     return (
-        <Flex h="1100px" direction="column" py="10" bg={ isDarkMode ? "gray.900" : "white"} alignItems="center">
+        <Flex h="1100px" direction="column" py="10" bg={isDarkMode ? "gray.900" : "white"} alignItems="center">
             <form onSubmit={handleSearch}>
                 <Stack spacing={[2, 4]} >
-            
-                        
-                    
                     <Stack direction={{ base: "row", lg: "row" }} justifyContent="center" padding={["20px", "30px", "40px"]}  >
                         <InputGroup size="lg" w={["100%", "600px"]} mx="auto">
-                            <Input type="text" name="search" borderRadius="8px" bozShadow="rgba(0,0,0,0.2) 0px 4px 6px 0px" placeholder="Search" color={isDarkMode ? "white" : "gray.800"} bg={isDarkMode ? "gray.800":"white"}/>
+                            <Input type="text" name="search" borderRadius="8px" bozShadow="rgba(0,0,0,0.2) 0px 4px 6px 0px" placeholder="Search" color={isDarkMode ? "white" : "gray.800"} bg={isDarkMode ? "gray.800" : "white"} />
                         </InputGroup>
-                        <Stack direction="row" justifyContent="center">
-                            <Button type="submit" px={[2, 4]} py={[3, 6]}><Icon as={FaSearch} w={6} h={10} color="gray.600"></Icon></Button>
+                        <Stack direction="row" align="center" justifyContent="center">
+                            <Button type="submit" px={[2, 4]} py={[5, 6]}><Icon as={FaSearch} w={6} h={10} color="gray.600"></Icon></Button>
                         </Stack>
                     </Stack>
                 </Stack>
             </form>
-            
-               { definitions.map((meaning, index) => (
-                    <div key={index}>
-                        <Stack  padding={["20px", "25px", "35px"]} bg={ isDarkMode ? "gray.900" : "white"} color={isDarkMode ? "white":"gray.700"} h="full">
-                            <Stack direction="column" justifyContent="flex-start">
-                                <Stack direction="row">
-                                    <Heading fontSize={["24px", "32px"]} color={isDarkMode ? "white":"black"} fontweight="900" lineHeight="taller">{word.charAt(0).toUpperCase() + word.slice(1)}</Heading>
-                                    <Heading fontSize={["24px", "32px"]} fontweight="700" lineHeight="taller">{phonetic}</Heading>
-                                </Stack>
-                                <Text fontSize={["16px", "20px"]} fontWeight="600">{meaning.partOfSpeech}</Text>
+
+            {definitions.map((meaning, index) => (
+                <div key={index}>
+                    <Stack padding={["20px", "25px", "35px"]} bg={isDarkMode ? "gray.900" : "white"} color={isDarkMode ? "white" : "gray.700"} h="full">
+                        <Stack direction="column" justifyContent="flex-start">
+                            <Stack direction="row">
+                                <Heading fontSize={["24px", "32px"]} color={isDarkMode ? "white" : "black"} fontweight="900" lineHeight="taller">{word.charAt(0).toUpperCase() + word.slice(1)}</Heading>
+                                <Heading fontSize={["24px", "32px"]} fontweight="700" lineHeight="taller">{phonetic}</Heading>
                             </Stack>
-                            <Stack >
-                                {meaning.definitions.map((definition, index) => (
-                                    <Text as="p" fontSize={["14px", "lg"]} fontFamily="Helvetica" key={index}> {definition.definition}</Text>
-                                ))}
-                            </Stack>
+                            <Text fontSize={["16px", "20px"]} fontWeight="600">{meaning.partOfSpeech}</Text>
                         </Stack>
-                    </div>
-                ))
-                                }            
+                        <Stack >
+                            {meaning.definitions.map((definition, index) => (
+                                <Text as="p" fontSize={["16px", "lg"]} fontWeight={["400","500"]} fontFamily="Monospace" key={index}> {definition.definition}</Text>
+                            ))}
+                        </Stack>
+                    </Stack>
+                </div>
+            ))
+            }
         </Flex>
     );
 }
